17943609f87bfe8c01f09ab72cf944fb47eac9a1
Joey Smeets <joeysmeetsss@gmail.com>
2022-01-23 15:51:40 +0100

    Changed the tests for the Absolute number method.

--- src/AbsoluteTest.java
+++ src/AbsoluteTest.java
@@ -10,19 +10,54 @@ class AbsoluteTest {
 	void setUp() throws Exception {
 		
 	}
-
+	
 	@Test
-	void testPositive() {
-		assertEquals(10, Absolute.absoluteValueOf(10));
-	}
-
-	@Test
-	void testNegative() {
-		assertEquals(7, Absolute.absoluteValueOf(-7));
+	void testNegativeBig() {
+		assertEquals(123456, Absolute.absoluteValueOf(-123456));
 	}
 	
 	@Test
-	void testNeutral() {
-		assertEquals("Testing the white box method", Absolute.absoluteValueOf("Testing the white box method")); 
+	void testNegative16() {
+		assertEquals(16, Absolute.absoluteValueOf(-16));
+	}
+	
+	@Test
+	void testNegative8() {
+		assertEquals(8, Absolute.absoluteValueOf(-8));
+	}
+	
+	@Test
+	void testMinusOne() {
+		assertEquals(1, Absolute.absoluteValueOf(-1));
+	}
+	
+	@Test
+	void testZero() {
+		assertEquals(0, Absolute.absoluteValueOf(0));
+	}
+
+	@Test
+	void testPositive8() {
+		assertEquals(8, Absolute.absoluteValueOf(8));
+	}
+	
+	@Test
+	void testPositive16() {
+		assertEquals(16, Absolute.absoluteValueOf(16));
+	}
+	
+	@Test
+	void testPositiveBig() {
+		assertEquals(123456, Absolute.absoluteValueOf(123456));
+	}
+	
+	@Test
+	void testChar() {
+		assertNotEquals('Testing the white box method', Absolute.absoluteValueOf('Testing the white box method')); 
+	}
+	
+	@Test
+	void testString() {
+		assertNotEquals("Testing the white box method", Absolute.absoluteValueOf("Testing the white box method")); 
 	}
 }
